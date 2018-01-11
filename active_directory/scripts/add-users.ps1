34b1eb2fe00826b2c6db09f43f59825c599f9ccb
Cris Tof <reg-github@geo6.net>
2018-01-11 14:59:55 +0100

    Update add-users.ps1

--- active_directory/scripts/add-users.ps1
+++ active_directory/scripts/add-users.ps1
@@ -1,4 +1,3 @@
-
 Import-module activedirectory
 
 $Users=Import-csv c:\scripts\add-users.csv
@@ -11,38 +10,38 @@ $usersAlreadyExist =@()
 $successUsers = @()
 $VerbosePreference = "Continue"
 $LogFolder = "c:\scripts\logs"
-$GroupName = "Bootcamp Users"
-$OU = "CN=Users, DC=TRAINING,DC=LOCAL"
+$GroupName = "training Users"
+$OU = "CN=Users, DC=training,DC=LOCAL"
 
 NEW-ADGroup -name $GroupName –GroupScope Global
 
-ForEach($User in $Users)
-{
- $User.FirstName = $User.FirstName.substring(0,1).toupper()+$User.FirstName.substring(1).tolower()
-   $FullName = $User.FirstName
-   $Sam = $User.FirstName 
-   $dnsroot = '@' + (Get-ADDomain).dnsroot
-   $SAM = $sam.tolower()
-   $UPN = $SAM + "$dnsroot"
-   $email = $Sam + "$dnsroot"
-   $password = $user.password
+ForEach($User in $Users){
+$User.FirstName = $User.FirstName.substring(0,1).toupper()+$User.FirstName.substring(1).tolower()
+$FullName = $User.FirstName
+$Sam = $User.FirstName
+$dnsroot = '@' + (Get-ADDomain).dnsroot
+$SAM = $sam.tolower()
+$UPN = $SAM + "$dnsroot"
+$email = $Sam + "$dnsroot"
+$password = $user.password
 try {
-    if (!(get-aduser -Filter {samaccountname -eq "$SAM"})){
-        New-ADUser -Name $FullName -AccountPassword (ConvertTo-SecureString $password -AsPlainText -force) -GivenName $User.FirstName  -Path $OU -SamAccountName $SAM -UserPrincipalName $UPN -EmailAddress $Email -Enabled $TRUE
-        Add-ADGroupMember -Identity $GroupName -Member $Sam
-        Write-Verbose "[PASS] Created $FullName"
-        $successUsers += $FullName
-    }
-  
+if (!(get-aduser -Filter {samaccountname -eq "$SAM"})){
+New-ADUser -Name $FullName -AccountPassword (ConvertTo-SecureString $password -AsPlainText -force) -GivenName $User.FirstName  -Path $OU -SamAccountName $SAM -UserPrincipalName $UPN -EmailAddress $Email -Enabled $TRUE
+Add-ADGroupMember -Identity $GroupName -Member $Sam
+Write-Verbose "[PASS] Created $FullName"
+$successUsers += $FullName
 }
+}
+
 catch {
-    Write-Warning "[ERROR]Can't create user [$($FullName)] : $_"
-    $failedUsers += $FullName
+Write-Warning "[ERROR]Can't create user [$($FullName)] : $_"
+$failedUsers += $FullName
 }
 }
+
 if ( !(test-path $LogFolder)) {
-    Write-Verbose "Folder [$($LogFolder)] does not exist, creating"
-    new-item $LogFolder -type directory -Force 
+Write-Verbose "Folder [$($LogFolder)] does not exist, creating"
+new-item $LogFolder -type directory -Force
 }
 
 Write-verbose "Writing logs"
